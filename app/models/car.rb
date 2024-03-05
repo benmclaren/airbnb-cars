330b48eb464ad2b28249597a919a24be8f15c35f
Ben McLaren <benmclaren@live.co.uk>
2024-03-05 16:08:16 +0000

    search added

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,11 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_manufacturer_model_and_address,
+    against: [ :manufacturer, :model, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
