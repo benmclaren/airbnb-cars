5be9da3c6db7c4d4788d6e57b25d51c31dec6829
Ben McLaren <benmclaren@live.co.uk>
2024-03-18 16:50:42 +0000

    flatpickr style and total price

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,11 +3,28 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
-  static targets = [ "startDate", "endDate" ]
+  static targets = [ "startDate", "endDate", "price", "pricePerDay" ]
   
   connect() {
-    console.log('connected');
-    flatpickr(this.startDateTarget, {})
-    flatpickr(this.endDateTarget, {})
+    flatpickr(this.startDateTarget, {
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+    })
+    flatpickr(this.endDateTarget, {
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+    })
+  }
+
+  calculateTotalPrice() {
+    const startDateValue = new Date(this.startDateTarget.value);
+    const endDateValue = new Date(this.endDateTarget.value);
+    const pricePerDay = this.pricePerDayTarget.innerText
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
+    const numberOfDays = Math.round((endDateValue - startDateValue) / millisecondsPerDay);
+    const totalPrice = numberOfDays * pricePerDay
+    this.priceTarget.innerText= totalPrice
   }
 }
